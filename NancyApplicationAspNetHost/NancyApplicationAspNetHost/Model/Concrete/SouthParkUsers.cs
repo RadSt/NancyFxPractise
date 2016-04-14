9ab754fa045b8bfaa2020ef17c87b7e7c6abc44e
RadSt <desan1986@gmail.com>
2016-04-14 22:11:06 +0300

    Add Nancy default bootstrapper class and configure it

---
+++ NancyApplicationAspNetHost/NancyApplicationAspNetHost/Model/Concrete/SouthParkUsers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NancyApplicationAspNetHost.Model.Abstract;
+
+namespace NancyApplicationAspNetHost.Model.Concrete
+{
+    public class SouthParkUsers : IUserStorage
+    {
+        public IList<dynamic> GetAllUsers()
+        {
+            return new List<dynamic>
+            {
+                new {Name ="Erik", SeccondName="Cartman", Age = 9},
+                new {Name = "Kenny", SecondName="McCormic", Age = 9},
+                new {Name = "Kyle", SecondName = "Broflovski", Age = 9},
+                new {Name = "Stan", SecondName = "March", Age = 9},
+                new {Name = "Butters", SecondName = "Stotch", Age = 9}
+            };
+        } 
+    }
+}
---
+++ NancyApplicationAspNetHost/NancyApplicationAspNetHost/MyBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using NancyApplicationAspNetHost.Model.Abstract;
+using NancyApplicationAspNetHost.Model.Concrete;
+
+namespace NancyApplicationAspNetHost
+{
+    public class MyBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            container.Register<IUserStorage, SouthParkUsers>();
+        }
+    }
+}
