7bc75d98dee4f83d21c00f8cf44c56a71832c5d0
Radik <desan1986@gmail.com>
2016-04-15 08:51:22 +0300

    Add Fiddler in headers Module. Set up proxy for Fiddler  in web.onfig

--- NancyApplicationAspNetHost/NancyApplicationAspNetHost/Modules/RouteModule.cs
+++ NancyApplicationAspNetHost/NancyApplicationAspNetHost/Modules/RouteModule.cs
@@ -1,11 +1,16 @@
-﻿namespace NancyApplicationAspNetHost.Modules
+﻿using System;
+using Nancy;
+
+namespace NancyApplicationAspNetHost.Modules
 {
     public class RouteModule : NancyFxModule
     {
+        private readonly Func<Request, bool> _isNotApiClient = request => 
+        !request.Headers.UserAgent.Equals("Fiddler"); 
         public RouteModule()
         {
             Get["/client"] = p => "Hello API user!";
-            Get["/client"] = p => View["Index.html"];
+            Get["/client", context => _isNotApiClient(context.Request)] = p => View["Index.html"];
         }
     }
 }
