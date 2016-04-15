5574cf04e6a449231b5f5015e68742252b0cae1b
Radik <desan1986@gmail.com>
2016-04-15 08:35:24 +0300

    Add Route module class with 2 modules

---
+++ NancyApplicationAspNetHost/NancyApplicationAspNetHost/Modules/RouteModule.cs
@@ -0,0 +1,11 @@
+﻿namespace NancyApplicationAspNetHost.Modules
+{
+    public class RouteModule : NancyFxModule
+    {
+        public RouteModule()
+        {
+            Get["/client"] = p => "Hello API user!";
+            Get["/client"] = p => View["Index.html"];
+        }
+    }
+}
