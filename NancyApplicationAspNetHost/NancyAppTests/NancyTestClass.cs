fbed3468105263f37b03650c9496d5792003d687
RadSt <desan1986@gmail.com>
2016-04-16 11:31:22 +0300

    Add Simple Nancy test and Libraries for it

---
+++ NancyApplicationAspNetHost/NancyAppTests/NancyTestClass.cs
@@ -0,0 +1,34 @@
+﻿using CsQuery;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy;
+using Nancy.Testing;
+using NancyApplicationAspNetHost;
+
+namespace NancyAppTests
+{
+    [TestClass]
+    public class NancyTestClass
+    {
+        [TestMethod]
+        public void Simplest_Get_Test()
+        {
+            var bootstrapper = new ConfigurableBootstrapper(with =>
+            {
+                with.Module<NancyFxModule>();
+                with.RootPathProvider<TestRootPathProvider>();
+            });
+
+            //Act
+            var browser = new Browser(bootstrapper);
+
+            //Arrange
+            var result = browser.Get("/", with =>
+            {
+                with.HttpRequest();
+            });
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        } 
+    }
+}
---
+++ NancyApplicationAspNetHost/NancyAppTests/TestRootPathProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy;
+
+namespace NancyAppTests
+{
+    public class TestRootPathProvider : IRootPathProvider
+    {
+        public string GetRootPath()
+        {
+            return @"D:\GIT\NancyFxPractise\NancyApplicationAspNetHost\NancyApplicationAspNetHost";
+        }
+    }
+}
