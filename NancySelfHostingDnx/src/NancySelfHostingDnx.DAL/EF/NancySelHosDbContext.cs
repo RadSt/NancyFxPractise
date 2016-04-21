fbc9c12e99d12c0ffc6e267acf9bac4553d11b7b
RadSt <desan1986@gmail.com>
2016-04-21 23:42:09 +0300

    Add UserProfile class

---
+++ NancySelfHostingDnx/src/NancySelfHostingDnx.DAL/EF/NancySelHosDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Entity;
+
+namespace NancySelfHostingDnx.DAL.EF
+{
+    public class NancySelHosDbContext : DbContext
+    {
+         protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ NancySelfHostingDnx/src/NancySelfHostingDnx.DAL/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NancySelfHostingDnx.DAL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NancySelfHostingDnx.DAL")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ad033f1a-3099-4c29-a6c1-59024ea65154")]
